ef7d3a555559f9e95f4cc6749adee5ec43bae34e
DeveshSinghAdhikari123 <devesh.adhikari123@gmail.com>
2024-05-20 16:47:52 +0530

    fixes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+
+def static
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create], defaults: {format: 'json'}
   resources :registrations, only: [:create]
+  root "home#static"
 
 end
